025ffc518901378eaf4337608455c10117623058
phuongninh1996 <phuongninh1996@gmail.com>
2015-09-22 20:13:57 -0500

    hola

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/3e/c0c678ee876100151908c3b77c1aa00b
@@ -0,0 +1,12 @@
+package com.airamerica;
+
+public class AirPort {
+	private String airportCode;
+	private String name;
+	private String Address;
+	private String coordinates;
+	private String passengerFacilityFee;
+	
+	
+	
+}
--- ProjectName/src/com/airamerica/OffSeasonTickets.java
+++ ProjectName/src/com/airamerica/OffSeasonTickets.java
@@ -1,5 +1,12 @@
 package com.airamerica;
 
 public class OffSeasonTickets extends ProductTypes{
-
+	private String seasonStartDate;
+	private String seasonEndDate;
+	private String depAirportCode; 
+	private String arrAirportCode;
+	private String depTime;
+	private String arrTime;
+	private fightNo
 }
+
